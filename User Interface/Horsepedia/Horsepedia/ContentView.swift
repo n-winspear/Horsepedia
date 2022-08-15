bbfa4d00761819306671a25d77b110441ba969de
n-winspear <n.winspear1@gmail.com>
2022-08-15 16:46:39 +0100

    Initial xCode setup

---
+++ User Interface/Horsepedia/Horsepedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Horsepedia
+//
+//  Created by Nathan Winspear on 15/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ User Interface/Horsepedia/Horsepedia/HorsepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorsepediaApp.swift
+//  Horsepedia
+//
+//  Created by Nathan Winspear on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HorsepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
